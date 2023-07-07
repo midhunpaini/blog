372bd208c83513326135b214c09679f8ed751952
Midhun krishna P <midhunpk2@gmail.com>
2023-07-07 16:11:10 +0530

    pathedit

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blog_app.urls')),
+    path('api/',include('blog_app.urls')),
     path('user/',include('user.urls'))
 ]
