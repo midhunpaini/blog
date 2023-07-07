0c72e391d01fa16e7c48303138d98b9a83d3ad53
Midhun krishna P <midhunpk2@gmail.com>
2023-07-07 17:00:16 +0530

    api

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/',include('blog_app.urls')),
-    path('user/',include('user.urls'))
+    path('api/user/',include('user.urls'))
 ]
